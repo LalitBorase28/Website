e9879cb2fe72fe8bf9c65198e5a2f8badfc222f9
LalitBorase28 <lalitpborse@gmail.com>
2025-12-29 10:50:51 +0530

    fix: update BrowserRouter basename and correct base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "khajuraho.staffhandler.com/webhooks/",
+  base: "/webhooks/",
   plugins: [react(), tailwindcss()],
 });
