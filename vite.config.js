b093204a28eec9b5506e150762714378937f420a
LalitBorase28 <lalitpborse@gmail.com>
2025-12-29 10:39:44 +0530

    update base URL in vite.config.js to point to the correct webhooks path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "khajuraho.staffhandler.com/webhooks/",
   plugins: [react(), tailwindcss()],
 });
